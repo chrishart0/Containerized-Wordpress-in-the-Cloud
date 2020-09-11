788c8152cc2642e188d059dcb4343fbae6163a6d
ChristianHart <christianphart0@gmail.com>
2020-09-10 22:40:55 -0400

    Add handling for DB in RDS

--- apacheWordpressDeploy.sh
+++ apacheWordpressDeploy.sh
@@ -13,43 +13,71 @@
 ############
 #Parameters#
 ############
-useParameterSotre=true
-serverName="test.arcadian.cloud"
+useRDS=true
+serverName="arcadian.cloud"
 serverAdminEmail="arcadiancloud@gmail.com"
-wordpressDB="wordpressDB"
-wordpressUser="wordpressUser"
 #Each CMK costs $1/month. Use the AWS Default key and don't be lazy with your IAM perms, make them specific to the Parameterstore key
 parameterStoreKeyId="alias/aws/ssm"
 AWStags="Key=SiteName,Value=$serverName Key=Project,Value=Blog"
 region=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone | egrep -o '(\w)+-(\w)+-[0-9]')
 
+#Optinal RDS Parameters #ToDo write lambda function which utalizes these keys to genereate SQL user specific to this wordpress DB. Can be called from here then parameters retreived. 
+RDSEndpointParameterStoreKey="/wordpress/sharedDB/Root/Endpoint"
+RDSRootUserParameterStoreKey="/wordpress/sharedDB/Root/User"
+RDSRootPasswordParameterStoreKey="/wordpress/sharedDB/Root/Pass"
+
+###########################
+
+#ToDo check that all needed parameters are provided 
+
 #consider using "set -e" this will error the whole script on any failure
 
-#Generate wordpress DB user password and root DB user password
+#Handle DB Users and Passwords
 #If no openssl use something like this:  cat /dev/urandom | tr -dc "A-Za-z0-9*+\-.,&%$!@^;~" | head -c14;
-#wordpressDbUserPass="adsfkjs13233409i7adf8"
-echo "Genereating random database passwords"
+echo "Genereating random database name, passwords, users"
+wordpressDB=("${serverName//.}$RANDOM")
+wordpressUser=("${serverName//.}$RANDOM")
 wordpressDbUserPass=$(openssl rand -base64 12)
-rootDbUserPass=$(openssl rand -base64 12)
+if [ "$useRDS" = true ];then 
+    echo "using RDS"
+else #Handle using local DB
+    echo "using local db"
+    rootDbUserPass=$(openssl rand -base64 12) 
+    
+fi
 
-function putParameterFailure {
-    printf "aws ssm put-parameter failed. Please correct the issue and try again. \nExiting..."
+function ssmParameterFailure {
+    printf "aws ssm command failed. Please correct the issue and try again. \nExiting..."
     exit 1
 }
 
+
+
+echo "Installing JQ for parameter handling"
+sudo yum install -y jq
+
+#Store DB passwords in parameter store
+#Parameter store standard is free, use it to your heart's content https://aws.amazon.com/systems-manager/pricing/
+# || after each ssm put-parameter only runs if there was some issue storing the parameter. It is very important to fail out if a parameter fails to get stored.
+#First check for parameter store permissions, if no parameter store permissions and no flag then exit
+#ToDo: Too much repition, see if this whole thing can be made into a function
 echo "Storing parameters in Parameter store"
-if [ "$useParameterSotre" = true ];then
-    #Store DB passwords in parameter store
-    #Parameter store standard is free, use it to your heart's content https://aws.amazon.com/systems-manager/pricing/
-    # || after each ssm put-parameter only runs if there was some issue storing the parameter. It is very important to fail out if a parameter fails to get stored.
-    #First check for parameter store permissions, if no parameter store permissions and no flag then exit
-    #ToDo: Too much repition, see if this whole thing can be made into a function
-    aws ssm put-parameter --name /wordpress/$serverName/db/wordpressDB --value $wordpressDB --type SecureString --key-id $parameterStoreKeyId --tags $AWStags --tier Standard --region $region --no-overwrite || putParameterFailure
-    aws ssm put-parameter --name /wordpress/$serverName/db/wordpressUser --value $wordpressUser --type SecureString --key-id $parameterStoreKeyId --tags $AWStags --tier Standard --region $region --no-overwrite || putParameterFailure
-    aws ssm put-parameter --name /wordpress/$serverName/db/wordpressDBUserPass --value $wordpressDbUserPass --type SecureString --key-id $parameterStoreKeyId --tags $AWStags --tier Standard --region $region --no-overwrite || putParameterFailure
-    aws ssm put-parameter --name /wordpress/$serverName/db/rootDbUserPass --value $rootDbUserPass --type SecureString --key-id $parameterStoreKeyId --tags $AWStags --tier Standard --region $region --no-overwrite || putParameterFailure
-    aws ssm put-parameter --name /wordpress/$serverName/info/serverName --value $serverName --type SecureString --key-id $parameterStoreKeyId --tags $AWStags --tier Standard --region $region --no-overwrite || putParameterFailure
-    aws ssm put-parameter --name /wordpress/$serverName/info/serverAdminEmail --value $serverAdminEmail --type SecureString --key-id $parameterStoreKeyId --tags $AWStags --tier Standard --region $region --no-overwrite || putParameterFailure
+aws ssm put-parameter --name /wordpress/$serverName/info/serverName --value $serverName --type SecureString --key-id $parameterStoreKeyId --tags $AWStags --tier Standard --region $region --no-overwrite || ssmParameterFailure
+aws ssm put-parameter --name /wordpress/$serverName/info/serverAdminEmail --value $serverAdminEmail --type SecureString --key-id $parameterStoreKeyId --tags $AWStags --tier Standard --region $region --no-overwrite || ssmParameterFailure
+
+if [ "$useRDS" = true ];then
+    #ToDo genereate local user and pass
+    aws ssm put-parameter --name /wordpress/$serverName/db/rds/wordpressDB --value $wordpressDB --type SecureString --key-id $parameterStoreKeyId --tags $AWStags --tier Standard --region $region --no-overwrite || ssmParameterFailure
+    aws ssm put-parameter --name /wordpress/$serverName/db/rds/wordpressUser --value $wordpressUser --type SecureString --key-id $parameterStoreKeyId --tags $AWStags --tier Standard --region $region --no-overwrite || ssmParameterFailure
+    aws ssm put-parameter --name /wordpress/$serverName/db/rds/wordpressDBUserPass --value $wordpressDbUserPass --type SecureString --key-id $parameterStoreKeyId --tags $AWStags --tier Standard --region $region --no-overwrite || ssmParameterFailure
+    RDSEndpoint=$(aws ssm get-parameter --name $RDSEndpointParameterStoreKey --with-decryption --region $region | jq '.Parameter.Value' | sed -e 's/^"//' -e 's/"$//') || ssmParameterFailure
+    rootDbUserUser=$(aws ssm get-parameter --name $RDSRootUserParameterStoreKey --with-decryption --region $region | jq '.Parameter.Value' | sed -e 's/^"//' -e 's/"$//') || ssmParameterFailure
+    rootDbUserPass=$(aws ssm get-parameter --name $RDSRootPasswordParameterStoreKey --with-decryption --region $region | jq '.Parameter.Value' | sed -e 's/^"//' -e 's/"$//') || ssmParameterFailure
+else
+    aws ssm put-parameter --name /wordpress/$serverName/db/local/wordpressDB --value $wordpressDB --type SecureString --key-id $parameterStoreKeyId --tags $AWStags --tier Standard --region $region --no-overwrite || ssmParameterFailure
+    aws ssm put-parameter --name /wordpress/$serverName/db/local/wordpressUser --value $wordpressUser --type SecureString --key-id $parameterStoreKeyId --tags $AWStags --tier Standard --region $region --no-overwrite || ssmParameterFailure
+    aws ssm put-parameter --name /wordpress/$serverName/db/local/wordpressDBUserPass --value $wordpressDbUserPass --type SecureString --key-id $parameterStoreKeyId --tags $AWStags --tier Standard --region $region --no-overwrite || ssmParameterFailure
+    aws ssm put-parameter --name /wordpress/$serverName/db/local/rootDbUserPass --value $rootDbUserPass --type SecureString --key-id $parameterStoreKeyId --tags $AWStags --tier Standard --region $region --no-overwrite || ssmParameterFailure
 fi
 
 ###############################
@@ -74,8 +102,8 @@ sudo yum update -y
 sudo yum upgrade -y
 
 #Use the amazon-linux-extras repo to install mariadb and php
-sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
-sudo yum install -y httpd mariadb-server php-gd php-fpm jq mod_ssl mod_http2
+sudo amazon-linux-extras install -y php7.2
+sudo yum install -y httpd php-gd php-fpm mod_ssl mod_http2 php-pecl-imagick #ImageMagick
 
 #Start apache service and set to atuostart
 echo "Starting Apache2"
@@ -105,20 +133,39 @@ echo "*/5 * * * * ec2-user /scripts/aws-scripts-mon/mon-put-instance-data.pl --m
 #Install and configure Wordpress. This section based on the below AWS documenation
 #https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/hosting-wordpress.html
 
-#Startup the DB and set it to run on startup
-echo "Starting MarisDB"
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
 
-#Configure root user password
-echo "Setting up DB"
-mysql -u root --password="" -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$rootDbUserPass');"
+if [ "$useRDS" = true ];then
+    #Install mysql so the tooling can be used to remotly connect
+    sudo yum install -y mysql
 
-#Configure MariaDB
-mysql -u root --password="$rootDbUserPass" -e "CREATE USER '$wordpressUser'@'localhost' IDENTIFIED BY '$wordpressDbUserPass';"
-mysql -u root --password="$rootDbUserPass" -e "CREATE DATABASE $wordpressDB;"
-mysql -u root --password="$rootDbUserPass" -e "GRANT ALL PRIVILEGES ON $wordpressDB.* TO '$wordpressUser'@localhost"
-mysql -u root --password="$rootDbUserPass" -e "FLUSH PRIVILEGES;"
+    #ToDo add test to confrim DB connectivity
+    #mysql -u $rootDbUserUser --password="$rootDbUserPass" -h $RDSEndpoint -e "SHOW DATABASES;"
+
+    #Configure RDS MySQL with new user
+    mysql -u $rootDbUserUser --password="$rootDbUserPass" -h $RDSEndpoint -e "CREATE USER '$wordpressUser'@'%' IDENTIFIED BY '$wordpressDbUserPass';"
+    mysql -u $rootDbUserUser --password="$rootDbUserPass" -h $RDSEndpoint -e "CREATE DATABASE $wordpressDB;"
+    mysql -u $rootDbUserUser --password="$rootDbUserPass" -h $RDSEndpoint -e "GRANT ALL PRIVILEGES ON $wordpressDB.* TO '$wordpressUser'@'%';"
+    mysql -u $rootDbUserUser --password="$rootDbUserPass" -h $RDSEndpoint -e "FLUSH PRIVILEGES;"
+else
+    #Install MariaDB
+    sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2
+    sudo yum install -y mariadb-server
+
+    #Startup the DB and set it to run on startup
+    echo "Starting MariaDB"
+    sudo systemctl start mariadb
+    sudo systemctl enable mariadb
+
+    #Configure root user password
+    echo "Setting up DB"
+    mysql -u root --password="" -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$rootDbUserPass');"
+
+    #Configure MariaDB
+    mysql -u root --password="$rootDbUserPass" -e "CREATE USER '$wordpressUser'@'localhost' IDENTIFIED BY '$wordpressDbUserPass';"
+    mysql -u root --password="$rootDbUserPass" -e "CREATE DATABASE $wordpressDB;"
+    mysql -u root --password="$rootDbUserPass" -e "GRANT ALL PRIVILEGES ON $wordpressDB.* TO '$wordpressUser'@localhost"
+    mysql -u root --password="$rootDbUserPass" -e "FLUSH PRIVILEGES;"
+fi
 
 #Download and unzip newest version of wordpress
 echo "Download and unzip newest version of wordpress"
@@ -132,6 +179,8 @@ cp wordpress/wp-config-sample.php wordpress/wp-config.php
 sed -i "s/database_name_here/$wordpressDB/" wordpress/wp-config.php
 sed -i "s/username_here/$wordpressUser/" wordpress/wp-config.php
 sed -i "s/password_here/$wordpressDbUserPass/" wordpress/wp-config.php
+if [ "$useRDS" = true ];then sed -i "s/localhost/$RDSEndpoint/" wordpress/wp-config.php fi
+#ToDo set host
 
 #Get salt values from wordpress and insert them into wp-config.php
 salts=$(curl https://api.wordpress.org/secret-key/1.1/salt/)
@@ -160,7 +209,7 @@ find /var/www -type f -exec sudo chmod 0664 {} \;
 sudo systemctl restart httpd 
 
 #Test that server is working
-echo "Testing Site
+echo "Testing Site"
 $siteTest=$(curl localhost/wp-admin/install.php)
 if [ -z $siteTest ];then {
     echo "Deployment has finished and wordpress is ready for human configuration"

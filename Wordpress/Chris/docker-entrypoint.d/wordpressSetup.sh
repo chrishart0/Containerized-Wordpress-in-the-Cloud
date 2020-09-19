f6a6218776e46dca817dd26982d54e3b571ca58c
ChristianHart <christianphart0@gmail.com>
2020-09-19 02:27:08 -0400

    done for the night

--- Wordpress/Chris/docker-entrypoint.d/wordpressSetup.sh
+++ Wordpress/Chris/docker-entrypoint.d/wordpressSetup.sh
@@ -1,7 +1,18 @@
 #bin/sh/
-mkdir /wordpress/ && cd /wordpress/
+cd /tmp
 curl -o wordpress.tar.gz -fSL "https://wordpress.org/wordpress-${WORDPRESS_VERSION}.tar.gz"
 echo "$WORDPRESS_SHA1 *wordpress.tar.gz" | sha1sum -c -
 tar -xzf wordpress.tar.gz
 rm wordpress.tar.gz
 
+#Delete default index.html
+rm $WEB_DIR/index.html
+cp -r /tmp/wordpress/* $WEB_DIR
+
+#Set Permissions
+chown -R nginx $WEB_DIR
+chgrp -R nginx $WEB_DIR
+chmod 2775 $WEB_DIR
+
+#Start php handler
+#php-fpm7
--- Wordpress/readme.md
+++ Wordpress/readme.md
@@ -2,7 +2,7 @@
 
 #Make sure you are in the directoru with the Dockerfile and run the following commands
 docker build -t container-wordpress .
-docker run -p 80:80 -t wordpress-container
+docker run -p 80:80 -t container-wordpress
 
 #To connect to a running container use the below command but change out <container name> with the container's name
 docker exec -it <container name> sh

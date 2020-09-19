a892222d31c060add730f49ead81605074af6c44
ChristianHart <christianphart0@gmail.com>
2020-09-19 00:38:43 -0400

    entrypoint working with nginx container

--- Wordpress/Chris/docker-entrypoint.d/wordpressSetup.sh
+++ Wordpress/Chris/docker-entrypoint.d/wordpressSetup.sh
@@ -1,5 +1,7 @@
 #bin/sh/
+mkdir /wordpress/ && cd /wordpress/
 curl -o wordpress.tar.gz -fSL "https://wordpress.org/wordpress-${WORDPRESS_VERSION}.tar.gz"
 echo "$WORDPRESS_SHA1 *wordpress.tar.gz" | sha1sum -c -
-tar -xzf wordpress.tar.gz -C /tmp
+tar -xzf wordpress.tar.gz
 rm wordpress.tar.gz
+
